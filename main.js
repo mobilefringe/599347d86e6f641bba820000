a158cbbb1667ed5117fd3bc2add3c6e756aeeee6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-15 15:36:38 -0400

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
           this.$store.commit('SET_LOCALE', { lang: value })
         }
       },
-      getBanners () {
+      getResults () {
           console.log(this.$store.state);
          return this.$store.state;
       }
