cc760a2b7b054269349fdc519a960847d693fdfd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-15 15:19:26 -0400

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
     },
     created() {
       // make an async call to the data store to initialize the locale (i.e. it will check if there is a locale value saved in cookie, otherwise it will default to EN)
+      this.getBanners;
       this.$store.dispatch('INITIALIZE_LOCALE');
       
       this.$store.dispatch('LOAD_META_DATA');
