4507b8d16ea6930b3259cdfff6c0fb7190756139
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-08-15 15:42:59 -0400

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
          return this.$store.state.results.banners;
       },
       getMainBanner () {
-          console.log( _.filter(this.getBanners, { 'position': 1})[0]);
+         console.log( _.filter(this.getBanners, { 'position': 1})[0]);
          return  _.filter(this.getBanners, { 'position': 1})[0];
       }
     },
