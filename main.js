2ef6a74f75653fef33433c54196541160bff6415
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-05 15:14:50 -0400

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -125,6 +125,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                 this.locale = val; // this will update the data store, which in turn will trigger the watcher to update the locale in the system
             },
             checkWidth: function() {
+                console.log("changing width")
                if(window.width() > 1200) {
                    this.showBigBanner = true;
                }
