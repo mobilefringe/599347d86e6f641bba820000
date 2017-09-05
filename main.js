4ec3e5d1c01ba9653d76800ec32cf94350599503
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-05 15:07:27 -0400

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
         data: function () {
             return {
                 dataLoaded: false,
-                test: '',
+                showBigBanner:false;
                 menu_items: [
                     {
                         name: "Stores",
