c5607f055b89eb5aff97418795a1de05a53bfa82
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-05 13:17:23 -0400

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -50,35 +50,35 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
         dataLoaded: false,
         test: '',
          menu_items: [
-                    {
-                        name: "Home",
-                        id: "home",
-                        class_list: "all_caps menu_btn_label",
-                        href: "/",
-                        is_inHome: false
-                    },
-                    {
-                        name: "Stores",
-                        id: "stores",
-                        class_list: "all_caps menu_btn_label",
-                        href: "/stores",
-                        is_inHome: true
-                    },
-                    {
-                        name: "Promotions",
-                        id: "promotions",
-                        class_list: "all_caps menu_btn_label",
-                        href: "/promotions",
-                        is_inHome: true
-                    },
-                    {
-                        name: "Path Map",
-                        id: "map",
-                        class_list: "all_caps menu_btn_label",
-                        href: "/map",
-                        is_inHome: true
-                    }
-                ]
+            {
+                name: "Home",
+                id: "home",
+                class_list: "all_caps menu_btn_label",
+                href: "/",
+                is_inHome: false
+            },
+            {
+                name: "Stores",
+                id: "stores",
+                class_list: "all_caps menu_btn_label",
+                href: "/stores",
+                is_inHome: true
+            },
+            {
+                name: "Promotions",
+                id: "promotions",
+                class_list: "all_caps menu_btn_label",
+                href: "/promotions",
+                is_inHome: true
+            },
+            {
+                name: "Path Map",
+                id: "map",
+                class_list: "all_caps menu_btn_label",
+                href: "/map",
+                is_inHome: true
+            }
+        ]
       }
     },
     created() {
