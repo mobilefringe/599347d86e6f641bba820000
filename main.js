9812293dbec73dd3f0decceb6d8de4cdb292e8d7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-05 15:52:11 -0400

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -144,7 +144,10 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                else {
                    this.showBigBanner = false;
                }
-            }
+            },
+            getWindowWidth(event) {
+        this.windowWidth = document.documentElement.clientWidth;
+      },
         },
         router: router,
         store,
