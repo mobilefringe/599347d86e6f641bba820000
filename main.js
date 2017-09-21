34e954b035ae35cca06757e872ca25fff14bb515
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-21 15:39:53 -0400

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -149,7 +149,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                 console.log(hours);
                 return hours;
             },
-            hours () {
+            today_hours () {
                 return this.$store.getters.getTodayHours;
             },
             timezone () {
