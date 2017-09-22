e635500672e25e111059ad1fcc6a0342a021adac
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-22 13:11:36 -0400

    Crimson Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -71,8 +71,6 @@
         </div>
         <div class="home_map">
             <iframe title="Map" width="100%" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  :src="'http://maps.google.nl/maps?q='+full_address  +'&amp;hl=en&amp;ie=UTF8&amp;t=v&amp;hnear='+full_address  +'&amp;z=13&amp;output=embed'">
-            <!--property.city property.country property.province_state property.province_state -->
-            <!--property.city property.country property.province_state property.province_state-->
                     Map
                 </iframe>
         </div>
