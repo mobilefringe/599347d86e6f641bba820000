a2fdd4213f9acc36aa6de0825aa3dce2071dd34e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-09-22 10:48:59 -0400

    Crimson Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -31,7 +31,7 @@
         </div>
          <div class="newsletter_div col-md-4">
             <form action="//mobilefringe.createsend.com/t/d/s/ithdul/" method="post" id="newsletter_form">
-                <input name="cm-ithdul-ithdul" type="text" placeholder="Enter E-mail Here" class="newsletter_control" required />
+                <input name="cm-ithdul-ithdul" type="text" placeholder="Enter E-mail Here" class="newsletter_control" required /><br/>
                 <button class="newsletter_btn animated_btn">Submit</button>
                 <p class="hidden_now" id="success_subscribe">
                     Thank you for subscribing.
