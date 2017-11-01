657697d2d2b06d715a00629cfa46edc447916e82
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-01 16:15:34 -0400

    Crimson Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -133,7 +133,7 @@
         feature_items(){
             console.log(this.$store.state.results);
             var features = _.slice(this.$store.state.results.feature_items, [start=0], [end=3]);
-            _.forEach({ 'a': 1, 'b': 2 }, function(value, key) {
+            _.forEach(features, function(value, key) {
               console.log(key);
             });
             return features;
