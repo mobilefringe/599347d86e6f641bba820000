b5e278e96d92031c05f4aaf229f0b5c96b370395
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-03 13:54:23 -0400

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -176,25 +176,25 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             },
             onOptionSelect(option) {
                 console.log('Selected option:', option);
-                var counted_stores = _.countBy(this.allStores,'name');
+                // var counted_stores = _.countBy(this.allStores,'name');
                 
-                // console.log("counted_stores is",counted_stores[option.name]);
-                if(option.type==="click") {
-                    console.log($(".input").val());
-                }
+                // // console.log("counted_stores is",counted_stores[option.name]);
+                // if(option.type==="click") {
+                //     console.log($(".input").val());
+                // }
                 
-                else {
-                    if( counted_stores[option.name] >1) {
-                        var route = '/map/' + option.name;
-                        console.log(route);
-                        this.$router.push(route);
-                    }
-                    else {
-                        var route = '/stores/' + option.slug;
-                        console.log(route);
-                        this.$router.push(route);
-                    }
-                }
+                // else {
+                //     if( counted_stores[option.name] >1) {
+                //         var route = '/map/' + option.name;
+                //         console.log(route);
+                //         this.$router.push(route);
+                //     }
+                //     else {
+                //         var route = '/stores/' + option.slug;
+                //         console.log(route);
+                //         this.$router.push(route);
+                //     }
+                // }
             }
         },
         router: router,
