39d40459447457b804c587c601710653631b5a0d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-13 14:35:16 -0500

    Crimson Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -164,6 +164,7 @@
                     this.map = map;
                 },
                 dropPin(store) {
+                    this.svgMapRefremoveMark(id);
                     this.svgMapRef.addMarker(store,'//codecloud.cdn.speedyrails.net/sites/595418c06e6f645d9d7c0000/image/png/1500567644000/map_pin_1x.png');
                 }
             },
