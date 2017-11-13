e63341b1fc1234b2ac364e3e898f450edb6dd4b5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-13 12:30:18 -0500

    Crimson Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -89,7 +89,7 @@
 </style>
 
 <script>
-  define(["Vue","vue!search-component"], function(Vue, SearchCompoentn) {
+  define(["Vue","vue!search-component"], function(Vue, SearchComponent) {
     return Vue.component("stores-component", {
       template: template, // the variable template will be injected
       data: function() {
