b75c22cf8178c245df2afa7acfbfbe757b4b676f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-13 17:28:26 -0500

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -84,6 +84,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                 ],
                 show_hero_banner : false,
                 suggestionAttribute: 'name',
+                banner_search : "",
                 search : ""
             }
         },
