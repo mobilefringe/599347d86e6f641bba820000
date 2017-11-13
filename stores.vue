68913609943b346632e93795eec54c7333e174c5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-13 12:56:45 -0500

    Crimson Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -40,8 +40,7 @@
                  <div class="store_list_container">
                     <div class="store-section" v-for="store in stores">
                         <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>
-                    
-                        </div>
+                    </div>
                  </div>
                  
             </div>
