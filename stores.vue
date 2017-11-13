61efb25d1871f2417de1a2dcbee01f68cef8dc62
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-13 12:34:57 -0500

    Crimson Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -45,15 +45,15 @@
     <!--  <li><a v-on:click="changeMode('category')">Category</a></li>-->
     <!--</ul>-->
     <hr/>
-    <div class="columns large-12" v-for="(stores, index) in storesByAlphaIndex" v-if="listMode === 'alphabetical'">
-      <div class="list_header">
-        <b>{{index}}</b>
-        <hr/>
-      </div>
-      <div class="store-section" v-for="store in stores">
-        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>
-        <hr/>
-      </div>
+    <!--<div class="columns large-12" v-for="(stores, index) in storesByAlphaIndex" v-if="listMode === 'alphabetical'">-->
+    <!--  <div class="list_header">-->
+    <!--    <b>{{index}}</b>-->
+    <!--    <hr/>-->
+    <!--  </div>-->
+    <!--  <div class="store-section" v-for="store in stores">-->
+    <!--    <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>-->
+    <!--    <hr/>-->
+    <!--  </div>-->
       <!-- <div class="card">
         <div class="card-divider">
           {{ store.name }}
@@ -67,17 +67,17 @@
           </div>
         </div>
       </div> -->
-    </div>
-    <div class="columns large-12" v-for="(stores, index) in storesByCategoryName" v-if="listMode === 'category'">
-      <div class="list_header">
-        <b>{{index}}</b>
-        <hr/>
-      </div>
-      <div class="store-section" v-for="store in stores">
-        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>
-        <hr/>
-      </div>
-    </div>
+    <!--</div>-->
+    <!--<div class="columns large-12" v-for="(stores, index) in storesByCategoryName" v-if="listMode === 'category'">-->
+    <!--  <div class="list_header">-->
+    <!--    <b>{{index}}</b>-->
+    <!--    <hr/>-->
+    <!--  </div>-->
+    <!--  <div class="store-section" v-for="store in stores">-->
+    <!--    <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>-->
+    <!--    <hr/>-->
+    <!--  </div>-->
+    <!--</div>-->
   </div>
 </template>
 
