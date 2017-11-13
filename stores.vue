dbaed8af02140541f0b0f5fb8876ac033057d57b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-13 12:42:08 -0500

    Crimson Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -95,7 +95,7 @@
 </style>
 
 <script>
-  define(["Vue","jquery", "Raphael", "mm_mapsvg","mousewheel","vue!search-component","vue!svg-map"], function(Vue, SearchComponent) {
+  define(["Vue","jquery", "Raphael", "mm_mapsvg","mousewheel","vue!search-component","vue!svg-map"], function(Vue, $, Raphael, mapSvg,mousewheel,SearchComponent,SVGMapComponent) {
     return Vue.component("stores-component", {
       template: template, // the variable template will be injected
       data: function() {
