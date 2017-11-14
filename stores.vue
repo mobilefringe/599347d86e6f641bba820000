c934a96f1c50bc8b4a7061dc4a7009a66d1cdf97
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-14 16:27:00 -0500

    Crimson Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -182,7 +182,7 @@
                 },
                 filterStores (letter) {
                     if(letter == "#"){
-                        this.processedStores = _.filter(this.allStores, function(o) { return _.inRange(_.toNumber(o.name[0]), -1, 10)); });
+                        this.processedStores = _.filter(this.allStores, function(o) { return _.inRange(_.toNumber(o.name[0]), -1, 10); });
                     }
                     else {
                         this.processedStores = _.filter(this.allStores, function(o) { return _.lowerCase(o.name[0]) == _.lowerCase(letter); });
