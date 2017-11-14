e55395e7b93b849b8f51520c1a332400e1d1343a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-14 16:24:14 -0500

    Crimson Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -184,6 +184,9 @@
                     if(letter == "#"){
                         
                     }
+                    else {
+                        
+                    }
                     this.processedStores = _.filter(this.allStores, function(o) { return _.lowerCase(o.name[0]) == _.lowerCase(letter); });
                     console.log(this.processedStores);
                 }
