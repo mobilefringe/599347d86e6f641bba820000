0be3f40038111eb02a747f66aa192f9d2172e156
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-14 17:55:33 -0500

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -184,7 +184,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             //this function helps home Page show and hide menu using the prop showMenu
             toggleBanner (payload) {
                 console.log("received emit");
-                this.show_hero_banner = val;
+                this.show_hero_banner = payload[0];
             },
             onOptionSelect(option) {
                 console.log('Selected option:', option);
