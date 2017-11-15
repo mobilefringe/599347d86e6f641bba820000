dbf6a90030fdad803a313da61289a0c70f4ff396
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-15 12:28:32 -0500

    Crimson Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -62,6 +62,24 @@
             methods: {
                 updateSVGMap (map) {
                     this.map = map;
+                },
+                property (){
+                    return this.$store.getters.getProperty;
+                },
+                storesByAlphaIndex() {
+                    return this.$store.getters.storesByAlphaIndex;
+                },
+                storesByCategoryName() {
+                    return this.$store.getters.storesByCategoryName;
+                },
+                getSVGurl () {
+                    return "https://www.mallmaverick.com" + this.property.svgmap_url;
+                },
+                allStores() {
+                    return this.$store.getters.processedStores;
+                },
+                svgMapRef() {
+                    return _.filter(this.$children, function(o) { return (o.$el.className == "svg-map") })[0];
                 }
             }
         });
