ea17182528e2345af03f61895b0d4d6dc48b892e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-15 12:27:03 -0500

    Crimson Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -54,11 +54,11 @@
             created (){
                 window.Raphael = Raphael; // our mapSvg plugin is stupid and outdated. need this hack to tie Raphael to window object (global variable)
             },
-        computed: {
-            findStoreBySlug () {
-                return this.$store.getters.findStoreBySlug;
+            computed: {
+                findStoreBySlug () {
+                    return this.$store.getters.findStoreBySlug;
+                }
             }
-        }
     });
   });
 </script>
