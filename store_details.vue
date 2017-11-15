016c8c92498e496be1cfe5df0b5140a755acab56
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-15 13:53:36 -0500

    Crimson Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -17,7 +17,7 @@
                 <img :src="currentStore.store_front_url_abs" class="" alt="">
                 <p></p>
                 <p></p>
-                {{currentStore.name}}
+                
                 {{currentStore.category_name}}
             </div>
             <div class="col-md-6">
