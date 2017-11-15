837ffc02d70cef3e4c424fa19d7e986ec3091804
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-15 14:32:16 -0500

    Crimson Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -84,6 +84,12 @@
             methods: {
                 updateSVGMap (map) {
                     this.map = map;
+                },
+                dropPin(){
+                    console.log(this.currentStore.svgmap_region);
+                    // this.svgMapRef.hideMarkers();
+                    this.svgMapRef.addMarker(this.currentStore,'//codecloud.cdn.speedyrails.net/sites/589e308f6e6f641b9f010000/image/png/1484850466000/show_pin.png');
+                    this.svgMapRef.setViewBox(this.currentStore)
                 }
                 
             }
