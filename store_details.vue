5f2d5a0bff69794f451e9c974e17310ae7dda00a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-15 13:52:54 -0500

    Crimson Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -24,6 +24,7 @@
                 </div>
             </div>
             <div class="col-md-3">
+            <p></p>
                 {{currentStore.phone}}
                 {{currentStore.website}}
                 {{currentStore.email}}
