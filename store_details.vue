29107e799c576cb23ca911c376b99669002eea3a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-15 12:25:39 -0500

    Crimson Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -51,7 +51,7 @@
           this.$router.replace({ name: '404'});
         }
       },
-      created (){
+        created (){
             window.Raphael = Raphael; // our mapSvg plugin is stupid and outdated. need this hack to tie Raphael to window object (global variable)
         },
       computed: {
