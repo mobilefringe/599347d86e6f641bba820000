d487969133ede1ebf73c4d2d2d5b4781fcb43574
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-15 10:20:30 -0500

    Crimson Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -3,8 +3,8 @@
         <div class="row">
             <div class="col-md-3">
                 {{currentStore.store_front_url_abs}}
-                 {{currentStore.store_front_url_abs}}
-                  {{currentStore.store_front_url_abs}}
+                {{currentStore.store_front_url_abs}}
+                {{currentStore.store_front_url_abs}}
             </div>
             <div class="col-md-6"></div>
             <div class="col-md-3"></div>
