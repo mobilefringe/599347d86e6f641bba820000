d766107e3808596c0b5e9c5a6253b13490f40a0d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-15 17:03:10 -0500

    Crimson Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -87,7 +87,7 @@
                       }, 500);
                 },
                 currentStore : function (){
-                    console.log("currentStore this",this);
+                    console.log("currentStore promo",this);
                     var vm = this;
                     var temp = [];
                     _.forEach(this.currentStore.promotions, function(value, key) {
