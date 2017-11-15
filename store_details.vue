65ef9ea360d30722eab1b60cf2094c05c2965364
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-15 12:29:16 -0500

    Crimson Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -56,9 +56,9 @@
             },
             mounted: {
                 this.svgMapRef.hideMarkers();
-                    console.log(store);
-                    this.svgMapRef.addMarker(store,'//codecloud.cdn.speedyrails.net/sites/589e308f6e6f641b9f010000/image/png/1484850466000/show_pin.png');
-                    this.svgMapRef.setViewBox(store)
+                console.log(store);
+                this.svgMapRef.addMarker(store,'//codecloud.cdn.speedyrails.net/sites/589e308f6e6f641b9f010000/image/png/1484850466000/show_pin.png');
+                this.svgMapRef.setViewBox(store)
             }
             computed: {
                 findStoreBySlug () {
