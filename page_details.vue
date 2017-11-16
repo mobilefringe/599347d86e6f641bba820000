8b4d22584b371ed84d8f5ba669e157574ddf28ba
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 14:34:19 -0500

    Crimson Template 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -3,7 +3,9 @@
          <div class="page_container padding_30" v-if="currentPage">
             <div class="page_title" v-html="currentPage.title">  </div>
             <div class="row">
-                
+                <div class="description_text" v-html="currentPage.body">
+                    
+                </div>
                 <!--<div class="col-md-4 ">-->
                 <!--    <div class="promo_img_container">-->
                 <!--        <img :src="currentEvent.image_url" class="promo_img">-->
