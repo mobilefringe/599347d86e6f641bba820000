19c1b72e37143df4f9c088fad5f66ace1889c0ff
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 10:18:18 -0500

    Crimson Template 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -4,11 +4,11 @@
         <div class="row"></div>
             <div class="col-md-3" v-for="promo in promotions">
                 <div class="">
-            <div class="card-divider">
-                <a :href="promo.image_url" target="_blank"><img :src="promo.image_url"></a>
-                {{ promo.name }}
-                
-            </div>
+                    <div class="card-divider">
+                        <a :href="promo.image_url" target="_blank"><img :src="promo.image_url"></a>
+                        {{ promo.name }}
+                        
+                    </div>
             <div class="">
               <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}" class="newsletter_btn animated_btn text_center">Read More</router-link>
             </div>
