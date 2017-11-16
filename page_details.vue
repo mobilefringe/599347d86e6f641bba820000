5e2073d6821008a529dcd4f6b10c7bb7a6be1cca
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 14:06:31 -0500

    Crimson Template 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -84,7 +84,7 @@
       },
       beforeRouteUpdate (to, from, next) {
             this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "pages/"+ to.params.id +".json"}).then(response => {
-                this.dataLoaded = true;
+                // this.dataLoaded = true;
             }, error => {
                 console.error("Could not retrieve data from server. Please check internet connection and try again.");
                 this.$router.replace({ name: '404'});
