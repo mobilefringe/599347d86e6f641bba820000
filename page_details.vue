29f9b34acafca3006861d00e15fbb9c4ff9cbde6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 14:05:59 -0500

    Crimson Template 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -75,10 +75,10 @@
             }, error => {
                 console.error("Could not retrieve data from server. Please check internet connection and try again.");
             });
-          vm.currentEvent = vm.findEventBySlug(to.params.id);
-          if (vm.currentEvent === null || vm.currentEvent === undefined){
-            vm.$router.replace({ name: '404'});
-          }
+            vm.currentEvent = vm.findEventBySlug(to.params.id);
+            if (vm.currentEvent === null || vm.currentEvent === undefined){
+                vm.$router.replace({ name: '404'});
+            }
         })
       },
       beforeRouteUpdate (to, from, next) {
