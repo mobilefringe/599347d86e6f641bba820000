ace4cbc5c0f32bd793660c3e59084b34dacf8300
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 10:15:48 -0500

    Crimson Template 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -15,7 +15,7 @@
             <div class="card-section">
               <div class="center">
                 
-                <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}">Read More</router-link>
+                <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}" class="newsltter_btn animated_btn">Read More</router-link>
               </div>
             </div>
           </div>
