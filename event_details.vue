656806c3483df1844f48a29babf6adede869ca18
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 12:26:25 -0500

    Crimson Template 1.0  /event_details.vue

---
+++ event_details.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+         <div class="page_container padding_30" v-if="currentPromo">
+            <div class="row">
+                <div class="col-md-4 ">
+                    <div class="promo_img_container">
+                        <img :src="currentPromo.image_url" class="promo_img">
+                    </div>
+                </div>
+                <div class="col-md-8 text_left promo_text_container">
+                    <div class="col-md-9">
+                        <p class="title all_caps">
+                            <router-link :to="{ name: 'storeDetails', params: { id: currentPromo.store.slug }}">{{currentPromo.store.name}}</router-link>
+                        </p>
+                        <p class="title">{{currentPromo.name}}</p>
+                        <br/>
+                        <p class="promo_dates"> {{currentPromo.start_date | moment("MMM D", timezone)}} - {{currentPromo.end_date | moment("MMM D", timezone)}}</p>
+                        <br/>
+                    </div>
+                    <div class="col-md-3">
+                        <img :src="currentPromo.store.store_front_url_abs" class="promo_store_logo" alt="">
+                    </div>
+                    <div class="col-md-12">
+                        <p class="description_text"> {{currentPromo.description}}</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <div class="page_container">
+            <div class="row  newsletter_subscription" style="margin:30px auto">
+                <div class="col-md-8 text_left">
+                    <h3 class="subscribe_heading all_caps">Subscribe to {{property.name}} newsletter</h3>
+                    <p class="subscribe_text">
+                        For Events, Promotions and Shopping Centre News<br/>
+                        Disclaimer: You will receive Promotion E-mails.
+                    </p>
+                </div>
+                 <div class="newsletter_div col-md-4 ">
+                    <form action="//mobilefringe.createsend.com/t/d/s/ithdul/" method="post" id="newsletter_form" class="pull-right">
+                        <input name="cm-ithdul-ithdul" type="text" placeholder="Enter E-mail Here" class="newsletter_control" required /><br/>
+                        <button class="newsletter_btn animated_btn all_caps ">Submit</button>
+                        <p v-show="success_subscribe" id="success_subscribe">
+                            Thank you for subscribing.
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <div style="padding:20px 0;"></div>
+    </div>
+</template>
+
+<script>
+  define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
+    return Vue.component("promo-details-component", {
+      template: template, // the variable template will be injected,
+      data: function() {
+        return {
+          currentPromo: null,
+          success_subscribe : false
+        }
+      },
+      beforeRouteEnter (to, from, next) {
+        next(vm => {
+          // access to component instance via `vm`
+          vm.currentPromo = vm.findPromoBySlug(to.params.id);
+          if (vm.currentPromo === null || vm.currentPromo === undefined){
+            vm.$router.replace({ name: '404'});
+          }
+        })
+      },
+      beforeRouteUpdate (to, from, next) {
+        this.currentPromo = this.findPromoBySlug(to.params.id);
+        if (this.currentPromo === null || this.currentPromo === undefined){
+          this.$router.replace({ name: '404'});
+        }
+      },
+      computed: {
+        findPromoBySlug () {
+          return this.$store.getters.findPromoBySlug;
+        },
+        timezone() {
+          return this.$store.getters.getTimezone;
+        },
+        property (){
+            return this.$store.getters.getProperty;
+        }
+      }
+    });
+  });
+</script>
