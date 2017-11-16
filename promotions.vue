b87d4ad1fb6a13dc7a7db678f0e95ef58aa6d924
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 11:02:01 -0500

    Crimson Template 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -1,7 +1,53 @@
 <template>
-<div>
-    
-</div>
+    <div>
+        <div class="page_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <div class="page_title"> Promotions </div>
+        <div class="row">
+            <div class="col-md-3" v-for="promo in promotions">
+                <div class="promo_list_container">
+                    <div class="promo_list_img_container">
+                        <!--<a :href="promo.image_url" target="_blank">-->
+                        <img :src="promo.image_url" class="promo_list_img">
+                            
+                        <!--</a>-->
+                    </div>
+                        <p class="sub_title">{{ promo.store.name }}</p>
+                        <p>{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
+                        <p class="description_text">{{ promo.name }}</p>
+                         <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}" class="newsletter_btn animated_btn text_center">Read More</router-link>
+                    
+                </div>
+            </div>
+        </div> 
+        <hr/>
+        <div class="new_stores page_container">
+            <div class="row">
+                <div class="col-md-2">
+                    <img src="http://assets.codecloudapp.com/sites/599347d86e6f641bba820000/image/png/1506100892000/Peoples.png" class="" alt="">
+                </div>
+                <div class="col-md-2">
+                    <img src="http://assets.codecloudapp.com/sites/599347d86e6f641bba820000/image/png/1506100902000/Roots.png" class="" alt="">
+                </div>
+                <div class="col-md-2">
+                    <img src="http://assets.codecloudapp.com/sites/599347d86e6f641bba820000/image/png/1506100908000/AA.png" class="" alt="">
+                </div>
+                <div class="col-md-2">
+                    <img src="http://assets.codecloudapp.com/sites/599347d86e6f641bba820000/image/png/1506100918000/UNQ.png" class="" alt="">
+                </div>
+                <div class="col-md-2">
+                    <img src="http://assets.codecloudapp.com/sites/599347d86e6f641bba820000/image/png/1506100932000/Best Buy.png" class="" alt="">
+                </div>
+                <div class="col-md-2">
+                    <img src="http://assets.codecloudapp.com/sites/599347d86e6f641bba820000/image/png/1506100952000/Indigo.png" class="" alt="">
+                </div>
+            </div>
+        </div>
+        <hr/>
+        <div class="row go_to_directory text_center">
+            <button class="newsletter_btn animated_btn all_caps ">Store Directory</button>
+        </div>
+    </div>
+    </div>
     
 </template>
 
