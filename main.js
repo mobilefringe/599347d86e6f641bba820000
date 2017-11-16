2585bdb0a44d952d766928e9fc857c9293cdb0ca
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 15:22:14 -0500

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -109,7 +109,9 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
               //Init
               this.getWindowWidth();
             });
-            this.$on('switchBanner', toggleBanner(value));
+            this.$on('toggleBanner', function(value){
+                console.log(value);
+            });
         },
         watch: {
             // watcher to update vue-i18n when the locale has been changed by the user
