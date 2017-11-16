c76a98cc9da740faf6d90ab5198f38fcb9b374b9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 10:57:12 -0500

    Crimson Template 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -10,7 +10,7 @@
                             
                         <!--</a>-->
                     </div>
-                        <p class="sub_title">{{ promo.store_name }}</p>
+                        <p class="sub_title">{{ promo.store.name }}</p>
                         <p>{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
                         <p class="description_text">{{ promo.name }}</p>
                          <router-link :to="{ name: 'promotionDetails', params: { id: promo.slug }}" class="newsletter_btn animated_btn text_center">Read More</router-link>
