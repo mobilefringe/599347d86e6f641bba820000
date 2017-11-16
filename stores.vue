f240e3c031fa553d18a578708dcd12392e273955
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 15:19:04 -0500

    Crimson Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+    <div class="page_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="col-md-12">
             <div class="alpha_list">
                 <!--<a @click="filterStores('7" id="all_stores_a">all</a>-->
