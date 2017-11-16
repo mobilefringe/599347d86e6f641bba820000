d0a0b95cfe3f254acdd249c701ec1bc7560e8457
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-16 14:01:02 -0500

    Crimson Template 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -58,7 +58,7 @@
 
 <script>
   define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
-    return Vue.component("promo-details-component", {
+    return Vue.component("page-details-component", {
       template: template, // the variable template will be injected,
       data: function() {
         return {
