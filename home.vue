ec1db0499602f41808e85c65011b8b219fb6fe4f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-22 15:44:30 -0500

    Crimson Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -104,6 +104,7 @@
         <div class="row go_to_directory text_center">
             <button class="newsletter_btn animated_btn all_caps ">Store Directory</button>
         </div>
+        </div>
     </div>
 </template>
 
