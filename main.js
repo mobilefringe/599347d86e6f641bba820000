35b3bc282620fca683c852b8b3f2f509a0e3947c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-24 12:31:34 -0500

    Crimson Template 1.0  /main.js

--- main.js
+++ main.js
@@ -136,7 +136,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             },
             $route : function () {
                 console.log("Changed to route:",this.$route.path);
-                this.show_menu = false
+                this.show_menu = falsel;
                 if(this.$route.path == "/") {
                     this.show_hero_banner = true;
                 }
