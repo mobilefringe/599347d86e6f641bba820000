3587e1aa810e850061b5ee511e82579674885c42
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-14 12:36:15 -0500

    Crimson Template 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -24,21 +24,21 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('email')}">
+                            <div class="col-sm-6 col-xs-12 text-left" :class="{'has-error': errors.has('email')}">
                                 <label class="label" for="email">Email</label>
                                 <input v-model="form_data.email" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email" placeholder="Email" data-vv-delay="1000">
                                 <span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('phone')}">
+                            <div class="col-sm-6 col-xs-12 text-left" :class="{'has-error': errors.has('phone')}">
                                 <label class="label" for="phone">Phone</label>
                                 <input v-model="form_data.phone" v-validate="'required|alpha_dash'" class="form-control" :class="{'input': true}" name="phone" type="phone" placeholder="Phone" data-vv-delay="1000">
                                 <span v-show="errors.has('phone')" class="form-control-feedback">{{ errors.first('phone') }}</span>
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-xs-12" :class="{'has-error': errors.has('message')}">
+                            <div class="col-xs-12 text-left" :class="{'has-error': errors.has('message')}">
                                 <label class="label" for="message">Message</label>
                                 <input v-model="form_data.message" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="message" type="text" placeholder="Message" data-vv-delay="1000">
                                 <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
