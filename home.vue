5eaa07544ba26b9256b313205c62b07ec5074f3c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-20 10:57:57 -0400

    Crimson Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -157,6 +157,7 @@
             template: template, // the variable template will be injected
             data: function() {
                 return {
+                    dataLoaded: false
                     success_subscribe: false
                 }
             },
