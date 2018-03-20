867fd32c595ff2b589cef5e649243933d6db5bb3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-20 10:51:51 -0400

    Crimson Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -149,7 +149,6 @@
                     'storesByCategoryName',
                     'findCategoryById',
                     'findCategoryByName',
-                    'findRepoByName'
 
                 ]),
                 getSVGurl () {
