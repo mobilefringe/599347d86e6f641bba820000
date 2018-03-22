cc7ca97de1da665e269cf513f84194abc5ac430e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-22 12:01:42 -0400

    Crimson Template 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -85,6 +85,16 @@
             // });
           return this.processedPromos;
         }
+      },
+      methods: {
+          loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch("getData", "promotions"), this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
       }
     });
   });
