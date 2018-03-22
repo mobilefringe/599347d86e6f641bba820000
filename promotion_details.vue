22f4fabdcf0656ce6022a079ced834cb4cb4645b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-22 12:35:12 -0400

    Crimson Template 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -63,21 +63,46 @@
           success_subscribe : false
         }
       },
-      beforeRouteEnter (to, from, next) {
-        next(vm => {
-          // access to component instance via `vm`
-          vm.currentPromo = vm.findPromoBySlug(to.params.id);
-          if (vm.currentPromo === null || vm.currentPromo === undefined){
-            vm.$router.replace({ name: '404'});
-          }
-        })
-      },
-      beforeRouteUpdate (to, from, next) {
-        this.currentPromo = this.findPromoBySlug(to.params.id);
-        if (this.currentPromo === null || this.currentPromo === undefined){
-          this.$router.replace({ name: '404'});
-        }
-      },
+      watch: {
+                currentPromo : function (){
+                    if(this.currentPromo != null) {
+                        console.log(this.currentPromo.store);
+                        if (this.currentPromo.store != null && this.currentPromo.store != undefined && _.includes(this.currentPromo.store.image_url, 'missing')) {
+                            this.currentPromo.store.image_url = this.property.default_logo_url;
+                        }
+                        else if (this.currentPromo.store == null || this.currentPromo.store == undefined) {
+                            this.currentPromo.store = {};
+                            this.currentPromo.store.image_url = this.property.default_logo_url;
+                        }
+                        var vm = this;
+                        var temp_promo = [];
+                        var current_id =_.toNumber(this.currentPromo.id);
+                        _.forEach(this.currentPromo.store.promotions, function(value, key) {
+                            if(_.toNumber(value) != current_id){
+                                var current_promo = vm.findPromoById(value);
+                                current_promo.description_short = _.truncate(current_promo.description, {'length': 70});
+                                temp_promo.push(current_promo);
+                            }
+                        });
+                        this.storePromos = temp_promo;
+                    }
+                    if(this.currentPromo.store) {
+                        var storeHours = [];
+                        var vm = this;
+                        _.forEach(this.currentPromo.store.store_hours, function (value, key) {
+                            var hour = vm.findHourById(value);
+                            if(hour.day_of_week === 0){
+                                hour.order = 7;
+                            }
+                            else {
+                                hour.order = hour.day_of_week;
+                            }
+                            storeHours.push();
+                        });
+                        this.storeHours = _.sortBy(storeHours, [function(o) { return o.order; }]);
+                    }
+                }
+            },
       computed: {
                 ...Vuex.mapGetters([
                     'property',
