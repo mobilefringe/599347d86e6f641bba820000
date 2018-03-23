c4bc6822ff378131c656fc9d1d682790e9039f2b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-23 11:45:26 -0400

    Crimson Template 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -1,32 +1,32 @@
 <template>
     <div>
-         <div class="page_container padding_30" v-if="currentEvent">
+         <div class="page_container padding_30" v-if="currentJob">
             <div class="row">
                 <div class="col-md-4 ">
                     <div class="promo_img_container">
-                        <!--<img :src="currentEvent.image_url" class="promo_img">-->
-                        <img src="//via.placeholder.com/250x250" class="promo_list_img" v-if="_.includes(currentEvent.image_url,'missing')">
-                        <img :src="currentEvent.image_url" class="promo_list_img" v-else>
+                        <!--<img :src="currentJob.image_url" class="promo_img">-->
+                        <img src="//via.placeholder.com/250x250" class="promo_list_img" v-if="_.includes(currentJob.image_url,'missing')">
+                        <img :src="currentJob.image_url" class="promo_list_img" v-else>
                     </div>
                 </div>
                 <div class="col-md-8 text_left promo_text_container">
                     <div class="col-md-9">
-                        <p class="title all_caps" v-if="currentEvent.store">
-                            <router-link :to="{ name: 'storeDetails', params: { id: currentEvent.store.slug }}">{{currentEvent.store.name}}</router-link>
+                        <p class="title all_caps" v-if="currentJob.store">
+                            <router-link :to="{ name: 'storeDetails', params: { id: currentJob.store.slug }}">{{currentJob.store.name}}</router-link>
                         </p>
                         <p class="title all_caps" v-else>
                            {{property.name}}
                         </p>
-                        <p class="title">{{currentEvent.name}}</p>
+                        <p class="title">{{currentJob.name}}</p>
                         <br/>
-                        <p class="promo_dates"> {{currentEvent.start_date | moment("MMM D", timezone)}} - {{currentEvent.end_date | moment("MMM D", timezone)}}</p>
+                        <p class="promo_dates"> {{currentJob.start_date | moment("MMM D", timezone)}} - {{currentJob.end_date | moment("MMM D", timezone)}}</p>
                         <br/>
                     </div>
-                    <div class="col-md-3" v-if="currentEvent.store">
-                        <img :src="currentEvent.store.store_front_url_abs" class="promo_store_logo" alt="">
+                    <div class="col-md-3" v-if="currentJob.store">
+                        <img :src="currentJob.store.store_front_url_abs" class="promo_store_logo" alt="">
                     </div>
                     <div class="col-md-12">
-                        <p class="description_text"> {{currentEvent.description}}</p>
+                        <p class="description_text"> {{currentJob.description}}</p>
                     </div>
                     
                 </div>
@@ -41,36 +41,36 @@
             template: template, // the variable template will be injected,
             data: function() {
                 return {
-                    currentEvent: null,
+                    currentJob: null,
                     success_subscribe: false
                 }
             },
             props: ['id'],
             beforeRouteUpdate(to, from, next) {
                 next();
-                this.updatecurrentEvent(to.params.id);
+                this.updatecurrentJob(to.params.id);
             },
             created() {
                 this.loadData().then(response => {
-                    this.updatecurrentEvent(this.id);
+                    this.updatecurrentJob(this.id);
                    
                     this.events = this.event;
                 });
             },
             watch: {
-                currentEvent: function() {
-                    if (this.currentEvent != null) {
-                        console.log(this.currentEvent.store);
-                        if (this.currentEvent.store != null && this.currentEvent.store != undefined && _.includes(this.currentEvent.store.image_url, 'missing')) {
-                            this.currentEvent.store.image_url = this.property.default_logo_url;
-                        } else if (this.currentEvent.store == null || this.currentEvent.store == undefined) {
-                            this.currentEvent.store = {};
-                            this.currentEvent.store.image_url = this.property.default_logo_url;
+                currentJob: function() {
+                    if (this.currentJob != null) {
+                        console.log(this.currentJob.store);
+                        if (this.currentJob.store != null && this.currentJob.store != undefined && _.includes(this.currentJob.store.image_url, 'missing')) {
+                            this.currentJob.store.image_url = this.property.default_logo_url;
+                        } else if (this.currentJob.store == null || this.currentJob.store == undefined) {
+                            this.currentJob.store = {};
+                            this.currentJob.store.image_url = this.property.default_logo_url;
                         }
                         var vm = this;
                         var temp_event = [];
-                        var current_id = _.toNumber(this.currentEvent.id);
-                        _.forEach(this.currentEvent.store.event, function(value, key) {
+                        var current_id = _.toNumber(this.currentJob.id);
+                        _.forEach(this.currentJob.store.event, function(value, key) {
                             if (_.toNumber(value) != current_id) {
                                 var current_event = vm.findEventById(value);
                                 current_event.description_short = _.truncate(current_event.description, {
@@ -95,9 +95,9 @@
                 ]),
             },
             methods: {
-                updatecurrentEvent(id) {
-                    this.currentEvent = this.findEventBySlug(id);
-                    if (this.currentEvent === null || this.currentEvent === undefined) {
+                updatecurrentJob(id) {
+                    this.currentJob = this.findEventBySlug(id);
+                    if (this.currentJob === null || this.currentJob === undefined) {
                         // this.$router.replace('/');
                     }
                 },
