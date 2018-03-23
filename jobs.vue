c419eb5ea2d34425298f187b2b6905d73093daa6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-23 13:33:44 -0400

    Crimson Template 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -81,8 +81,8 @@
                     var vm = this;
                     var temp_promo = [];
                     _.forEach(this.processedJobs, function(value, key) {
-                        today = moment().tz(vm.timezone).format('MM DD YYYY');
-                        webDate = moment(value.show_on_web_date).tz(vm.timezone).format('MM DD YYYY');
+                        today = moment().tz(vm.timezone);
+                        webDate = moment(value.show_on_web_date).tz(vm.timezone);
                         console.log("compare",today, ">=", webDate, (today >= webDate))
                         if (today >= webDate) {
                             value.description_short = _.truncate(value.description, {
