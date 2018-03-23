fba273b77726477f796919491f731c09610153a7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-23 11:44:58 -0400

    Crimson Template 1.0  /job_details.vue

---
+++ job_details.vue
@@ -0,0 +1,115 @@
+<template>
+    <div>
+         <div class="page_container padding_30" v-if="currentEvent">
+            <div class="row">
+                <div class="col-md-4 ">
+                    <div class="promo_img_container">
+                        <!--<img :src="currentEvent.image_url" class="promo_img">-->
+                        <img src="//via.placeholder.com/250x250" class="promo_list_img" v-if="_.includes(currentEvent.image_url,'missing')">
+                        <img :src="currentEvent.image_url" class="promo_list_img" v-else>
+                    </div>
+                </div>
+                <div class="col-md-8 text_left promo_text_container">
+                    <div class="col-md-9">
+                        <p class="title all_caps" v-if="currentEvent.store">
+                            <router-link :to="{ name: 'storeDetails', params: { id: currentEvent.store.slug }}">{{currentEvent.store.name}}</router-link>
+                        </p>
+                        <p class="title all_caps" v-else>
+                           {{property.name}}
+                        </p>
+                        <p class="title">{{currentEvent.name}}</p>
+                        <br/>
+                        <p class="promo_dates"> {{currentEvent.start_date | moment("MMM D", timezone)}} - {{currentEvent.end_date | moment("MMM D", timezone)}}</p>
+                        <br/>
+                    </div>
+                    <div class="col-md-3" v-if="currentEvent.store">
+                        <img :src="currentEvent.store.store_front_url_abs" class="promo_store_logo" alt="">
+                    </div>
+                    <div class="col-md-12">
+                        <p class="description_text"> {{currentEvent.description}}</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment"], function(Vue, Vuex, moment, tz, VueMoment) {
+        return Vue.component("event-details-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    currentEvent: null,
+                    success_subscribe: false
+                }
+            },
+            props: ['id'],
+            beforeRouteUpdate(to, from, next) {
+                next();
+                this.updatecurrentEvent(to.params.id);
+            },
+            created() {
+                this.loadData().then(response => {
+                    this.updatecurrentEvent(this.id);
+                   
+                    this.events = this.event;
+                });
+            },
+            watch: {
+                currentEvent: function() {
+                    if (this.currentEvent != null) {
+                        console.log(this.currentEvent.store);
+                        if (this.currentEvent.store != null && this.currentEvent.store != undefined && _.includes(this.currentEvent.store.image_url, 'missing')) {
+                            this.currentEvent.store.image_url = this.property.default_logo_url;
+                        } else if (this.currentEvent.store == null || this.currentEvent.store == undefined) {
+                            this.currentEvent.store = {};
+                            this.currentEvent.store.image_url = this.property.default_logo_url;
+                        }
+                        var vm = this;
+                        var temp_event = [];
+                        var current_id = _.toNumber(this.currentEvent.id);
+                        _.forEach(this.currentEvent.store.event, function(value, key) {
+                            if (_.toNumber(value) != current_id) {
+                                var current_event = vm.findEventById(value);
+                                current_event.description_short = _.truncate(current_event.description, {
+                                    'length': 70
+                                });
+                                temp_event.push(current_event);
+                            }
+                        });
+                        this.storeEvents = temp_event;
+                    }
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'processedEents',
+                    'findEventBySlug',
+                    'findEventById',
+                    'timezone',
+                    'findRepoByName',
+                    'findHourById'
+                ]),
+            },
+            methods: {
+                updatecurrentEvent(id) {
+                    this.currentEvent = this.findEventBySlug(id);
+                    if (this.currentEvent === null || this.currentEvent === undefined) {
+                        // this.$router.replace('/');
+                    }
+                },
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch("getData", "events"), this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                }
+            }
+        });
+    });
+</script>
