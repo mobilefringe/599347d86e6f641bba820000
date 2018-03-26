a4e000035ca459cf8710639bfafe1c4c5629f51d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-26 10:15:32 -0400

    Crimson Template 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
 	    <a href="#"><i class="fa fa-twitter"></i></a>
 	    <a href="#"><i class="fa fa-pinterest"></i></a>
 	    <a href="#"><i class="fa fa-instagram"></i></a>
