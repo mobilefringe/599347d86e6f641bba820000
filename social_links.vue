8ca1f21816058e0d2c8b2ed6f16e2e07649a7eac
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-26 10:15:42 -0400

    Crimson Template 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
-	    <a href="#"><i class="fa fa-twitter"></i></a>
-	    <a href="#"><i class="fa fa-pinterest"></i></a>
-	    <a href="#"><i class="fa fa-instagram"></i></a>
-        <a href="#"><i class="fa fa-google-plus"></i></a>
+	    <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+	    <a href="#" target="_blank"><i class="fa fa-pinterest"></i></a>
+	    <a href="#" target="_blank"><i class="fa fa-instagram"></i></a>
+        <a href="#" target="_blank"><i class="fa fa-google-plus"></i></a>
     </div>
     
 </template>
