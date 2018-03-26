dec0ae62b7d2bdc67206fc8b0940237be00b7fff
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-26 11:26:47 -0400

    Crimson Template 1.0  /subscription_box.vue

--- subscription_box.vue
+++ subscription_box.vue
@@ -20,3 +20,21 @@
 <!--                        </div>-->
 <!--                    </div>-->
 <!--                </div>-->
+<template>
+    <div>
+        <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+	    <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+	    <a href="#" target="_blank"><i class="fa fa-pinterest"></i></a>
+	    <a href="#" target="_blank"><i class="fa fa-instagram"></i></a>
+        <a href="#" target="_blank"><i class="fa fa-google-plus"></i></a>
+    </div>
+    
+</template>
+<script>
+    define(["Vue", "vuex", "lightbox"], function(Vue, Vuex, Lightbox) {
+        Vue.use(Lightbox);
+        return Vue.component("social-links", {
+            template: template, // the variable template will be injected,
+        });
+    });
+</script>
