e8dfb8c07888b92a55790169234ab5886ca31f44
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-03 16:41:15 -0400

    Crimson Template 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -62,8 +62,8 @@
                         There was an error when trying to submit your request. Please try again later.
                     </div>
                     
-                </div>
-            <!--</div>-->
+                <!--</div>-->
+            </div>
             <div class="padding_top_40"></div>    
         </div>
     </div>
