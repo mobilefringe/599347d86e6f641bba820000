44768d1d406ec298ce9c3dfe279d5483a90f91b4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-03 15:08:46 -0400

    Crimson Template 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -111,6 +111,18 @@
                     return this.getTodayHours;
                 }
             },
+            methods: {
+                onOptionSelect(option) {
+                console.log('Selected option:', option);
+                this.$router.push("/stores/"+option.slug);
+                $(".bannerSearch .options-list").hide();
+                this.$nextTick(function() {
+                    //clear search when changing routes
+                    this.search = "";
+                    this.menuSearch = "";
+                });
+            },
+            }
             
         });
     });
