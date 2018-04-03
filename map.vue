e4b89419d828aa9f6024af60a03c5a362b4e4d21
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-03 17:15:10 -0400

    Crimson Template 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -34,7 +34,7 @@
                 </div>
             </div>
             <div class="stores_container">
-                <div class="col-md-4 col-sm-4">
+                <!--<div class="col-md-4 col-sm-4">-->
                     <!--<div class="search_container hidden_phone">-->
                     <!--    <search-component :list="processedStores" placeholder="Find Your Store" :suggestion-attribute="suggestionAttribute" v-model="storeSearch" @select="onOptionSelect">-->
                     <!--        <template slot="item" scope="option">-->
@@ -57,18 +57,8 @@
                     <!--        </template>-->
                     <!--    </search-component>-->
                     <!--</div>-->
-                    <div class="store_list_container">
-                        <div class="store-section hidden_phone" v-for="store in filteredStores">
-                            <a @click="dropPin(store)">{{store.name}}</a>
-                        </div>
-                        <div class="store-section visible_phone" v-for="store in filteredStores">
-                            <a @click="onOptionSelectPhone(store)">{{store.name}}</a>
-                        </div>
-                       <div class="store-section" v-if="filteredStores.length <= 0">
-                            No stores avalaible.
-                        </div>
-                    </div>
-                </div>
+                    
+                <!--</div>-->
                 <div class="col-md-8 col-sm-8">
                     <div class="map_container full_border hidden_phone">
                         <mapplic-map ref="mapplic_ref" :height="740" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="processedStores" :floorlist="floorList" :svgWidth="2000" :svgHeight="2000" tooltiplabel="Info"></mapplic-map>
